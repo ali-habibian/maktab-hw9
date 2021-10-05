b5139d3c88d6efe78fc6e059b4ce045f909c4cf8
Ali Habibian <ali.habibian04@gmail.com>
2021-10-06 02:57:35 +0330

    q3 completed

--- src/q3/CustomLinkedList.java
+++ src/q3/CustomLinkedList.java
@@ -1,4 +1,77 @@
 package q3;
 
-public class CustomLinkedList {
+public class CustomLinkedList<E> {
+    CustomNode head;
+
+
+    class CustomNode {
+        E data;
+        CustomNode next;
+
+        public CustomNode(E data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public void put(E data) {
+        CustomNode newCustomNode = new CustomNode(data);
+
+        if (head == null) {
+            head = newCustomNode;
+            return;
+        }
+
+        newCustomNode.next = null;
+
+        CustomNode last = head;
+        while (last.next != null) {
+            last = last.next;
+        }
+
+        last.next = newCustomNode;
+    }
+
+    public void deleteNode(E data) {
+        CustomNode temp = head, prev = null;
+
+        if (temp != null && temp.data == data) {
+            head = temp.next;
+            return;
+        }
+
+        while (temp != null && temp.data != data) {
+            prev = temp;
+            temp = temp.next;
+        }
+
+        // If data was not present in linked list
+        if (temp == null)
+            return;
+
+        // Unlink the node from linked list
+        prev.next = temp.next;
+    }
+
+    public boolean search(E data) {
+        CustomNode current = head;
+        while (current != null) {
+            if (current.data == data)
+                return true;
+            current = current.next;
+        }
+        return false;
+    }
+
+    public boolean isEmpty(){
+        return head == null;
+    }
+
+    public void printList() {
+        CustomNode node = head;
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
 }
--- src/q3/Main.java
+++ src/q3/Main.java
@@ -2,6 +2,31 @@ package q3;
 
 public class Main {
     public static void main(String[] args) {
+        CustomLinkedList<Integer> list = new CustomLinkedList<>();
 
+        list.put(1);
+        list.put(2);
+        list.put(6);
+        list.put(4);
+        list.put(5);
+        list.printList();
+
+        list.deleteNode(6);
+        System.out.println();
+        System.out.println("After delete 6");
+        list.printList();
+
+        System.out.println();
+        System.out.print("Search for 2: ");
+        if (list.search(2))
+            System.out.println("Found");
+        else
+            System.out.println("Not Found");
+
+        System.out.println();
+        if (list.isEmpty())
+            System.out.println("List is empty");
+        else
+            System.out.println("List is not empty");
     }
 }
